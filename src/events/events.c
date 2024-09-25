bde540f0f43be629541bb5c8f147c71e89ad396c
Daiana Rotchen <darotche@c4b9c1.42berlin.de>
2024-09-25 18:27:35 +0200

    close X button

--- src/events/events.c
+++ src/events/events.c
@@ -10,6 +10,8 @@ int	handle_no_event(void *data)
 int	close_window(t_data *data)
 {
 	mlx_destroy_window(data->mlx_ptr, data->win_ptr);
+	mlx_destroy_display(data->mlx_ptr);
+	free(data->mlx_ptr);
 	exit (0);
 }
 // int	handle_keypress(int keysym, t_data *data)
